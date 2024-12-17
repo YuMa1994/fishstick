c0a8546adada9523a1c26b92146616784d205c13
YuMa1994 <yulianna-markvald@yandex.ru>
2024-12-17 22:58:41 +0200

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,23 @@
+def send_email(message, recipient, sender="university.help@gmail.com"):
+    if '@' not in recipient or '@' not in sender or not sender.endswith(('.com', '.ru', '.net')) or not recipient.endswith(('.com', '.ru', '.net')):
+        return 'Невозможно отправить письмо с адреса <sender> на адрес <recipient>'
+    elif sender == recipient:
+        return 'Нельзя отправить письмо самому себе!'
+    elif sender == 'university.help@gmail.com':
+        return 'Письмо успешно отправлено с адреса <sender> на адрес <recipient>.'
+    else:
+        return 'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <sender> на адрес <recipient>.'
+
+print(send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com'))
+print(send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com'))
+print(send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk'))
+print(send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru'))
+
+# Пункты задачи:
+# Создайте функцию send_email, которая принимает 2 обычных аргумента: message(сообщение), recipient(получатель) и 1 обязательно именованный аргумент со значением по умолчанию sender = "university.help@gmail.com".
+# Если строки recipient и sender не содержит "@" или не оканчивается на ".com"/".ru"/".net", то вывести на экран(в консоль) строку: "Невозможно отправить письмо с адреса <sender> на адрес <recipient>".
+# Если же sender и recipient совпадают, то вывести "Нельзя отправить письмо самому себе!"
+# Если же отправитель по умолчанию - university.help@gmail.com, то вывести сообщение: "Письмо успешно отправлено с адреса <sender> на адрес <recipient>."
+# В противном случае вывести сообщение: "НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <sender> на адрес <recipient>."
+# Здесь <sender> и <recipient> - значения хранящиеся в этих переменных.
+# За один вызов функции выводится только одно и перечисленных уведомлений! Проверки перечислены по мере выполнения.
