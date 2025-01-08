9ea98fb1fa95cfb175ad3226ef84598f45f6ea9a
YuMa1994 <yulianna-markvald@yandex.ru>
2025-01-08 16:10:53 +0200

    Add files via upload

---
+++ main14-1.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from time import sleep
+import datetime
+
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+file = open('log.txt', 'w')
+option = webdriver.ChromeOptions()
+option.add_experimental_option('detach', True)
+#option.add_argument('--headless')
+driver = webdriver.Chrome(options=option)
+
+driver.get('https://demoqa.com/checkbox')
+driver.maximize_window()
+
+def expand_list():
+    main_list_expand = driver.find_element(By.XPATH, '//*[@id="tree-node"]/div/button[1]')
+    main_list_expand.click()
+
+def home_check_box():
+    home_check_box_click = driver.find_element(By.XPATH, '//*[@id="tree-node"]/ol/li/span/label/span[1]')
+    home_check_box_click.click()
+
+
+# Tests
+
+def test_context_after_expand_list():
+    lowest_list_element_text = 'Excel File.doc'
+    current_text = driver.find_element(By.XPATH, '//*[@id="tree-node"]/ol/li/ol/li[3]/ol/li[2]/span/label/span[3]')
+    assert lowest_list_element_text == current_text.text, 'test_context_after_expand_list if FAILED'
+    file.write('test_context_after_expand_list is OK \n')
+
+def test_context_after_home_checkbox_click():
+    correct_text = 'You have selected :'
+    current_text = driver.find_element(By.XPATH, '//*[@id="result"]/span[1]')
+    assert correct_text == current_text.text, 'test_context_after_home_checkbox_click is FAILED'
+    file.write('test_context_after_home_checkbox_click is OK')
+
+expand_list()
+test_context_after_expand_list()
+home_check_box()
+test_context_after_home_checkbox_click()
+
+file.close()
+#driver.quit()
+
